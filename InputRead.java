b8e4956b381e6108fc14e473470f3f80b24a6655
Luiz Lage <luizhenrique455@gmail.com>
2023-10-29 22:22:31 -0300

    Add filtro de stopwords

--- InputRead.java
+++ InputRead.java
@@ -6,13 +6,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+class Frase {
+    List<String> listaPalavras;
+    Frase(List<String> listaPalavras) {
+        this.listaPalavras = listaPalavras;
+    }
+}
 class Artigo {
     String titulo;
     String resumo;
     String tags;
     String[] frases;
+    List<Frase> frasesFiltradas;
+    List<String> vertices;
 
     public Artigo(String titulo, String resumo, String tags) {
         this.titulo = titulo;
@@ -29,6 +38,9 @@ public class InputRead {
     static File resumePath = new File(
             InputRead.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "resumos");
 
+    // Dicionario de stopwords
+    static List<String> dicionarioStopWords = new ArrayList<>(Arrays.asList("a", "as", "da", "das", "o", "os", "do", "dos", "e", "em", "de", "no", "nos", "na", "nas", "ante", "apos", "ate", "com", "contra", "desde", "entre", "para", "por", "perante", "sob", "sobre", "deste", "destes", "este", "estes", "esta", "estas", "desta", "destas", "esse", "essa", "isto", "isso", "se", "sendo", "tambem", "que"));
+
     //caso o programa não pegue o path correto automaticamente (é para funcionar no windows), insira os paths nas variaveis a baixo
     public static void setupPath() {
         if (resumePath.list() == null) {
@@ -42,7 +54,7 @@ public class InputRead {
     public static void main(String[] args) throws IOException {
         List<Artigo> artigos = new ArrayList<>();
         List<Artigo> artigosNormalizados = new ArrayList<>();
-        setupPath();
+        setupPath(); // Configura caminho para o repositorio para ler os arquivos .txt
 
         for (String file : listFiles()) {
             if (file.toString().contains(".txt")) {
@@ -50,9 +62,9 @@ public class InputRead {
                 try {
                     Artigo artigoAtual = readFiles(path);
                     artigos.add(artigoAtual);
-                    print("lido:" + file);
+                    print("lido:" + file); // debug de quais arquivos foram lidos
                 } catch (Exception e) {
-                    print("Arquivo não lido:" + file);
+                    print("Arquivo não lido:" + file); // debug de quais arquivos nao foram lidos
                     print(e.getLocalizedMessage());
                 }
             }
@@ -62,7 +74,10 @@ public class InputRead {
             Artigo normalized = normalizeArtigo(artigo);
             artigosNormalizados.add(normalized);
         }
-        
+
+        for (Artigo artigo : artigosNormalizados) {
+            filtrarStopWords(artigo);
+        }        
     }
 
     public static String[] listFiles() {
@@ -81,11 +96,10 @@ public class InputRead {
         Path path = Paths.get(fileName);
         List<String> allLines = Files.readAllLines(path, charset);
 
-        //imprime todos os textos
-        // for (String line : allLines) {
-            //System.out.println(line);
-        //}
+        //imprime todos as linhas dos textos
+        //print(allLines);
 
+        //linha(0) == titulo // linha(1) == resumo // linha(2) == tags 
         Artigo artigo = new Artigo(allLines.get(0), allLines.get(1), allLines.get(2));
 
         return artigo;
@@ -98,12 +112,68 @@ public class InputRead {
         artigoAux.resumo = Normalizer.normalize(artigoAux.resumo, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", ""); // faz o replace de todos caracteres especiais como acentos
         artigoAux.resumo = artigoAux.resumo.replaceAll("[^\\w\\s.]", ""); // remove todas as pontuações
 
-        //imprime frases
-        // for(String frase : artigo.frases) {
-        //     print(frase);
-        // }
+        //imprime frases todas as frases
+        //print(artigo.frases);
 
         return new Artigo(artigo.titulo, artigo.resumo, artigo.tags);
+    }
+
+    public static Artigo filtrarStopWords(Artigo artigo) {
+        List<Frase> frasesResult = new ArrayList<>();
+
+        for (String frase : artigo.frases) {
+            Frase fraseFiltrada = FiltrarFrase(frase);
+            frasesResult.add(fraseFiltrada);
+        }
+        artigo.frasesFiltradas = frasesResult;
+
+        // // DEBUG comparando frase original com a frase filtrada
+        // print("================");
+        // print(artigo.frases);
+        // print("================");
+        // for (Frase frase: frasesResult) {
+        //     printAsText(frase);
+        // }
+        // print("================");
+
+        return artigo;
+    }
+
+    public static Frase FiltrarFrase(String fraseInteira) {
+        List<String> resultList = new ArrayList<>();
+        String[] frase = fraseInteira.split(" ");
+        for (String palavra : frase) {
+            if(!dicionarioStopWords.contains(palavra)) {
+                resultList.add(palavra);
+            }
+        }
+        return new Frase(resultList);
+    }
+
+    // Metodos print para facilitar a vida...
+    public static void print(String[] msg) {
+        for(String value: msg) {
+            print(value);
+        }
+    }
+
+    public static void print(List<String> msg) {
+        if(msg == null) return;
+        msg.forEach((v) -> print(v));
+    }
+
+    public static void printFrase(List<Frase> a) {
+        if(a == null) return;
+        a.forEach((v) -> print(v));
+    }
+
+    public static void printAsText(Frase frase) {
+        frase.listaPalavras.forEach((v) -> System.out.print(v + " "));
+    }
+
+    public static void print(Frase msg) {
+        if(msg == null) return;
+        print(msg.listaPalavras);
     }
 
     public static void print(String msg) {
